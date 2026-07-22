b57dd831dca957e280ef0355f4bcca6c6d1a4e9a
Vmkmanoj <vmkmanoj13@gmail.com>
2026-07-22 13:47:41 +0530

    engine added

--- app/database/engine.py
+++ app/database/engine.py
@@ -1,25 +1,37 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-DATA_BASE = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATA_BASE)
-
-sessionLocal = sessionmaker(
-    autoflush=False,
-    autocommit = False,
-    bind = engine
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
 )
 
-def get_db():
-    db = sessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-    
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# DATABASE_URL example:
+# postgresql+asyncpg://postgres:password@localhost:5432/hotel_booking
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
+
+async def get_db():
+    async with SessionLocal() as db:
+        try:
+            yield db
+        finally:
+            await db.close()
